3c0b0d08421257cd4568814a0bed438e9e972ab2
Anna Wilson <ajoyna01@gmail.com>
2021-05-29 08:15:22 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+const main = document.querySelector('#main');
+
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute("id" ,"victory");
+
+newHeader.innerHTML = "Anna is the champion!"
+
+document.body.append(newHeader)
+
+
